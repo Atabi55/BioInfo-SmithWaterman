7b53fe0707d1992fa9e33d675ee295a604458db3
Atabi55 <ataguler55@gmail.com>
2025-05-28 13:41:17 +0300

    (Fixed) Empty score matrix creation

--- smith_waterman.py
+++ smith_waterman.py
@@ -8,14 +8,26 @@ def smith_waterman(seq1, seq2, match=2, mismatch=-1, gap=-2, use_blosum62=False)
     #Default values: match=2, mismatch=-1, gap=-2, use_blosum62=False
     #Output: List of all local alignments with inserted gaps
 
-    #matrix: 2 dimensional list to hold the score for each cell
-    matrix = []
-    #fill 0th row and 0th column with all 0
+
+    #matrix: 2-dimensional list to hold score of each cell
+    #seq1 in vertical axis, seq2 in horizontal axis
+    matrix = [[None for i in range(len(seq2)+1)] for j in range(len(seq1)+1)]
+
+    # fill 0th row and 0th column with all 0
     for i in range(len(seq1)+1):
         matrix[i][0] = 0
-    for i in range(len(seq2)+2):
+    for i in range(len(seq2)+1):
         matrix[0][i] = 0
 
+    print_2d_list(matrix)#test
+
+
+
+
+#helper function to print matrix
+def print_2d_list(matrix):
+    for i in range(len(matrix)):
+        print(matrix[i])
 
 
 
